df2e0b6d2164d6a214fca35cb634d31971ca262a
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2024-08-26 21:30:15 +0200

    fix text persistence

--- src/app/countdown.service.ts
+++ src/app/countdown.service.ts
@@ -1,77 +1,81 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountdownService {
-  private endDate: Date | undefined;
-  private eventName: string | undefined;
-  private timer$ = new BehaviorSubject<string>('');
+  private endDate: Date | undefined
+  private eventName: string | undefined
+  private timer$ = new BehaviorSubject<string>('')
+  private eventName$ = new BehaviorSubject<string>('')
 
   constructor() {
-    this.loadFromStorage();
-    this.startCountdown();
+    this.loadFromStorage()
+    this.startCountdown()
   }
 
   setEvent(name: string, date: Date) {
-    this.eventName = name;
-    this.endDate = date;
-    this.saveToStorage();
-    this.startCountdown();
+    this.eventName = name
+    this.endDate = date
+    this.saveToStorage()
+    this.startCountdown()
   }
 
   getTimer() {
-    return this.timer$.asObservable();
+    return this.timer$.asObservable()
+  }
+
+  getEventName() {
+    return this.eventName$.asObservable()
   }
 
   private calculateTimeLeft() {
-    const now = new Date().getTime();
+    const now = new Date().getTime()
     if (!this.endDate) {
-      return '';
+      return ''
     }
-    const end = new Date(this.endDate).getTime();
-    const timeLeft = end - now;
+    const end = new Date(this.endDate).getTime()
+    const timeLeft = end - now
 
     if (timeLeft <= 0) {
-      this.timer$.next("It's time");
-      return "It's time";
+      this.timer$.next("It's time")
+      return "It's time"
     }
 
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
     const hours = Math.floor(
       (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    );
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    )
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
 
-    return `${days} days, ${hours} h, ${minutes}m, ${seconds}s`;
+    return `${days} days, ${hours} h, ${minutes}m, ${seconds}s`
   }
 
   private startCountdown() {
     if (this.endDate) {
       setInterval(() => {
-        this.timer$.next(this.calculateTimeLeft());
-      }, 1000);
+        this.timer$.next(this.calculateTimeLeft())
+      }, 1000)
     }
   }
 
   private saveToStorage() {
-    if (this.eventName) {
-      localStorage.setItem('eventName', this.eventName);
-    }
-    if (this.endDate) {
-      localStorage.setItem('endDate', this.endDate.toString());
+    if (this.eventName && this.endDate) {
+      localStorage.setItem('eventName', this.eventName)
+      localStorage.setItem('endDate', this.endDate.toString())
     }
   }
 
   private loadFromStorage() {
-    const storedName = localStorage.getItem('eventName');
-    const storedDate = localStorage.getItem('endDate');
+    const storedName = localStorage.getItem('eventName')
+    const storedDate = localStorage.getItem('endDate')
 
     if (storedName && storedDate) {
-      this.eventName = storedName;
-      this.endDate = new Date(storedDate);
+      this.eventName = storedName
+      this.endDate = new Date(storedDate)
+      this.eventName$.next(storedName)
     }
   }
 }
--- src/app/event-name-display/event-name-display.component.ts
+++ src/app/event-name-display/event-name-display.component.ts
@@ -6,7 +6,8 @@ import {
   ElementRef,
   Renderer2,
   ViewChild,
-} from '@angular/core';
+} from '@angular/core'
+import { CountdownService } from '../countdown.service'
 
 @Component({
   selector: 'app-event-name-display',
@@ -14,53 +15,60 @@ import {
   styleUrls: ['./event-name-display.component.scss'],
 })
 export class EventNameDisplayComponent implements OnChanges {
-  @Input() eventName: string = '';
-  fontSize: string = '2rem';
+  @Input() eventName: string = ''
+  fontSize: string = '2rem'
 
   @ViewChild('eventText', { static: false }) divEventText:
     | ElementRef
-    | undefined;
+    | undefined
 
   constructor(
     private el: ElementRef,
     private renderer: Renderer2,
+    private countdownService: CountdownService,
   ) {}
+
+  ngOnInit() {
+    this.countdownService.getEventName().subscribe((eventName) => {
+      this.eventName = eventName
+    })
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['eventName']) {
-      this.adjustFontSize();
+      this.adjustFontSize()
     }
   }
 
   adjustFontSize() {
-    const length = this.eventName.length;
-    let fontSize: number; // Base size in rem
+    const length = this.eventName.length
+    let fontSize: number // Base size in rem
 
     switch (true) {
       case length === 0:
-        fontSize = 5;
-        break;
+        fontSize = 5
+        break
       case length <= 10:
-        fontSize = 5;
-        break;
+        fontSize = 5
+        break
       case length <= 20:
-        fontSize = 3;
-        break;
+        fontSize = 3
+        break
       case length <= 30:
-        fontSize = 2;
-        break;
+        fontSize = 2
+        break
       case length <= 40:
-        fontSize = 1.5;
-        break;
+        fontSize = 1.5
+        break
       case length <= 50:
-        fontSize = 1;
-        break;
+        fontSize = 1
+        break
       default:
-        fontSize = 0.9;
-        break;
+        fontSize = 0.9
+        break
     }
 
-    this.fontSize = `${fontSize}rem`;
+    this.fontSize = `${fontSize}rem`
 
     // Check if `divEventText` is available before applying styles
     if (this.divEventText) {
@@ -68,10 +76,10 @@ export class EventNameDisplayComponent implements OnChanges {
         this.divEventText.nativeElement,
         'font-size',
         this.fontSize,
-      );
+      )
     } else {
       // Alternatively, apply styles to the host element
-      this.renderer.setStyle(this.el.nativeElement, 'font-size', this.fontSize);
+      this.renderer.setStyle(this.el.nativeElement, 'font-size', this.fontSize)
     }
   }
 }
