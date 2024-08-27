17217035748a8897c7b4f0d1df7016f445ee755e
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2024-08-27 10:15:27 +0200

    alert for max length

--- src/app/event-input/event-input.component.ts
+++ src/app/event-input/event-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, HostListener } from '@angular/core';
-import { CountdownService } from '../countdown.service';
-import { NgForm } from '@angular/forms';
+import { Component, HostListener } from '@angular/core'
+import { CountdownService } from '../countdown.service'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-event-input',
@@ -8,42 +8,43 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./event-input.component.scss'],
 })
 export class EventInputComponent {
-  eventName: string = '';
-  endDate: string = '';
+  eventName: string = ''
+  endDate: string = ''
 
   constructor(private countdownService: CountdownService) {}
 
-  @HostListener('window:keydown.enter', ['$event'])
-  onEnter(event: KeyboardEvent) {
-    event.preventDefault();
-    this.submit();
-  }
-
   submit() {
     if (!this.eventName || !this.endDate) {
-      alert('Please fill in both fields.');
-      return;
+      alert('Please fill in both fields.')
+      return
     }
 
-    const date = new Date(this.endDate);
+    const date = new Date(this.endDate)
 
     if (isNaN(date.getTime())) {
-      alert('Invalid date format.');
-      return;
+      alert('Invalid date format.')
+      return
     }
 
-    const now = new Date();
+    const now = new Date()
     if (date <= now) {
-      alert('The end date must be in the future.');
-      return;
+      alert('The end date must be in the future.')
+      return
     }
 
-    this.countdownService.setEvent(this.eventName, date);
+    this.countdownService.setEvent(this.eventName, date)
   }
 
   handleEnter(form: NgForm) {
     if (form.valid) {
-      this.submit();
+      this.submit()
+    }
+  }
+
+  alertForMaxLength(event: Event) {
+    const input = event.target as HTMLInputElement
+    if (input.value.length === input.maxLength) {
+      alert('Maximum character limit reached.')
     }
   }
 }
